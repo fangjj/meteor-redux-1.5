f1f13ef7d267a9919030f3985fd89de1b08b3bc7
= <fang_jimjim@163.com>
2017-06-24 15:15:57 +0800

    test ssh

--- imports/ui/component/Task.jsx
+++ imports/ui/component/Task.jsx
@@ -10,7 +10,5 @@ export default class Task extends Component {
 }
 
 Task.PropTypes = {
-    // This component gets the task to display through a React prop.
-    // We can use propTypes to indicate it is required
     task: PropTypes.object.isRequired,
 };
